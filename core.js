0f23779a16f0ff5d6fc97eb59d2bf810bda2dc55
Asuka <154034002+Love-Asuka@users.noreply.github.com>
2025-11-23 00:34:27 +0800

    改个名字

---
+++ core.js
@@ -0,0 +1,16 @@
+class main {
+  getInfo() {
+    return {
+      id: 'EtudeTurboWarpML',
+      name: 'Etude-TurboWarp-ML',
+      author: 'Asuka | Lin Xin',
+      version: '1.0.1',
+
+      blocks: []
+    };
+  }
+  
+
+}
+
+Scratch.extensions.register(new main());
