f3f22709b00ff9c56fc9a58e1f0f9047e9b75aab
Asuka <154034002+Love-Asuka@users.noreply.github.com>
2025-11-23 00:53:51 +0800

    注册一下

---
+++ autograd.js
@@ -0,0 +1,13 @@
+class autograd {
+  getInfo() {
+    return {
+      id: 'EtudeTurboWarpMLAutograd',
+      name: 'Etude-TurboWarp-ML_Autograd',
+      author: 'Asuka | Lin Xin',
+      version: '1.0.1',
+
+      blocks: []
+    };
+  }
+}
+Scratch.extensions.register(new autograd());
---
+++ optimizer.js
@@ -0,0 +1,13 @@
+class optimizer {
+  getInfo() {
+    return {
+      id: 'EtudeTurboWarpMLOptimizer',
+      name: 'Etude-TurboWarp-ML_Optimizer',
+      author: 'Asuka | Lin Xin',
+      version: '1.0.1',
+
+      blocks: []
+    };
+  }
+}
+Scratch.extensions.register(new optimizer());
